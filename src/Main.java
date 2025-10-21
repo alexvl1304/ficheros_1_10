12680d315f74f753a2fc836868364c187a1abe97
alexvl <alexxi3000.0o0@gmail.com>
2025-10-21 14:34:18 +0200

    validator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
+import org.xml.sax.SAXException;
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.File;
 
 void main() {
@@ -10,6 +16,10 @@ void main() {
     System.out.println(list.getList().get(0).getName());
 
     Marshalling(list);
+
+    System.out.println(validarXMLconXSD("salida.xml","full_person.xsd"));
+
+    System.out.println(validarXMLconXSD("ejemplo.xml","full_person.xsd"));
 }
 
 public boolean Marshalling(Individuals list) {
@@ -43,4 +53,24 @@ public Individuals Unmarshalling(String ruta) {
         e.printStackTrace();
         return null;
     }
+}
+
+
+public static boolean validarXMLconXSD(String xmlPath, String xsdPath) {
+    try {
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+
+        Schema schema = factory.newSchema(new File(xsdPath));
+
+        Validator validator = schema.newValidator();
+
+        validator.validate(new StreamSource(new File(xmlPath)));
+
+        return true;
+    } catch (SAXException e) {
+        System.out.println("El XML no es válido: " + e.getMessage());
+    } catch (Exception e) {
+        System.out.println("Error durante la validación: " + e.getMessage());
+    }
+    return false;
 }
